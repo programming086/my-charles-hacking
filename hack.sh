4b809058d8dacc4b59fb9f08b472dddb73dab0ae
Roman Brovko <brovko-roman@ya.ru>
2017-04-21 20:50:59 +0300

    add support 4.1.1

--- hack.sh
+++ hack.sh
@@ -3,13 +3,13 @@ DIR=charleshack
 
 mkdir $DIR
 cd $DIR
-cat >> GFjZ.java <<EOF
+cat >> slZe.java <<EOF
 package com.xk72.charles;
-public final class GFjZ {
-    public static boolean VxNA() { return true; }
-    public static String eSXN() { return "Administrator"; }
-    public static String VxNA(String paramString1, String paramString2) { return null; }
+public final class slZe {
+    public static boolean RrCt() { return true; }
+    public static String Vvni() { return "Administrator"; }
+    public static String RrCt(String paramString1, String paramString2) { return null; }
 }
 EOF
-javac -encoding UTF-8 GFjZ.java -d . && jar -uvf $CHARLES com/xk72/charles/GFjZ.class
+javac -encoding UTF-8 slZe.java -d . && jar -uvf $CHARLES com/xk72/charles/slZe.class
 cd .. && rm -rf $DIR
