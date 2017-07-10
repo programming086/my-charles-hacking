0309b7e2a650419b1ffc1b9634805e3d5d6309ae
Roman Brovko <brovko-roman@ya.ru>
2017-07-10 18:58:19 +0300

    add support 4.1.4

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 # Official Download Link
 
-- [Windows 64 bit](https://www.charlesproxy.com/assets/release/4.1.3/charles-proxy-4.1.3-win64.msi)
-- [Windows 32 bit](https://www.charlesproxy.com/assets/release/4.1.3/charles-proxy-4.1.3-win32.msi)
-- [macOS](https://www.charlesproxy.com/assets/release/4.1.3/charles-proxy-4.1.3.dmg)
-- [Linux 64/32 bit](https://www.charlesproxy.com/assets/release/4.1.3/charles-proxy-4.1.3.tar.gz)
+- [Windows 64 bit](https://www.charlesproxy.com/assets/release/4.1.4/charles-proxy-4.1.4-win64.msi)
+- [Windows 32 bit](https://www.charlesproxy.com/assets/release/4.1.4/charles-proxy-4.1.4-win32.msi)
+- [macOS](https://www.charlesproxy.com/assets/release/4.1.4/charles-proxy-4.1.4.dmg)
+- [Linux 64/32 bit](https://www.charlesproxy.com/assets/release/4.1.4/charles-proxy-4.1.4.tar.gz)
--- hack.sh
+++ hack.sh
@@ -3,14 +3,14 @@ DIR=charleshack
 
 mkdir $DIR
 cd $DIR
-cat >> psPJ.java <<EOF
+cat >> gIbD.java <<EOF
 package com.xk72.charles;
-public final class psPJ {
-    public static boolean qIvM() { return true; }
-    public static void mLFE() {}
-    public static String tCiz() { return "Administrator"; }
-    public static String qIvM(String paramString1, String paramString2) { return null; }
+public final class gIbD {
+    public static boolean xUFT() { return true; }
+    public static void QNfW() {}
+    public static String PcqR() { return "Administrator"; }
+    public static String xUFT(String paramString1, String paramString2) { return null; }
 }
 EOF
-javac -encoding UTF-8 psPJ.java -d . && jar -uvf $CHARLES com/xk72/charles/psPJ.class
+javac -encoding UTF-8 gIbD.java -d . && jar -uvf $CHARLES com/xk72/charles/gIbD.class
 cd .. && rm -rf $DIR
