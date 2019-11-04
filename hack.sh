e509bb90e2948bfc991381e0bb998a9da52f7578
Roman Brovko <brovko-roman@ya.ru>
2019-11-04 20:09:08 +0200

    add support 4.5.4

--- hack.sh
+++ hack.sh
@@ -3,15 +3,15 @@ DIR=charleshack
 
 mkdir $DIR
 cd $DIR
-cat >> dGpN.java <<EOF
+cat >> DIWy.java <<EOF
 package com.xk72.charles;
-public final class dGpN {
-    public static boolean LLzr() { return true; }
-    public static void UKki() {}
-    public static String fGTB() { return "Administrator"; }
-    public static String LLzr(String paramString1, String paramString2) { return null; }
+public final class DIWy {
+    public static boolean OZtq() { return true; }
+    public static void Bgcz() {}
+    public static String wkKg() { return "Administrator"; }
+    public static String OZtq(String paramString1, String paramString2) { return null; }
 }
 EOF
-javac -encoding UTF-8 dGpN.java -d . && jar -uvf $CHARLES com/xk72/charles/dGpN.class
+javac -encoding UTF-8 DIWy.java -d . && jar -uvf $CHARLES com/xk72/charles/DIWy.class
 cd .. && rm -rf $DIR
 xattr -d com.apple.quarantine /Applications/Charles.app/
