5f2e9f07f9ab0d843bc7c1ab4430122a0be681f6
Roman Brovko <brovko-roman@ya.ru>
2020-01-14 19:22:38 +0200

    add support 4.5.6

--- hack.sh
+++ hack.sh
@@ -3,15 +3,15 @@ DIR=charleshack
 
 mkdir $DIR
 cd $DIR
-cat >> ymZn.java <<EOF
+cat >> Bvcn.java <<EOF
 package com.xk72.charles;
-public final class ymZn {
-    public static boolean VPvq() { return true; }
-    public static void beZk() {}
-    public static String Inbu() { return "Administrator"; }
-    public static String VPvq(String paramString1, String paramString2) { return null; }
+public final class Bvcn {
+    public static boolean FwRs() { return true; }
+    public static void GSXJ() {}
+    public static String InuZ() { return "Administrator"; }
+    public static String FwRs(String paramString1, String paramString2) { return null; }
 }
 EOF
-javac -encoding UTF-8 ymZn.java -d . && jar -uvf $CHARLES com/xk72/charles/ymZn.class
+javac -encoding UTF-8 Bvcn.java -d . && jar -uvf $CHARLES com/xk72/charles/Bvcn.class
 cd .. && rm -rf $DIR
 xattr -d com.apple.quarantine /Applications/Charles.app/
