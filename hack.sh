127b200b1a31df2d69365212b04c8c4bc3a227da
Roman Brovko <brovko-roman@ya.ru>
2020-11-07 19:36:35 +0200

    add support 4.6

--- hack.sh
+++ hack.sh
@@ -3,15 +3,18 @@ DIR=charleshack
 
 mkdir $DIR
 cd $DIR
-cat >> Bvcn.java <<EOF
+cat >> FWZE.java <<EOF
 package com.xk72.charles;
-public final class Bvcn {
-    public static boolean FwRs() { return true; }
-    public static void GSXJ() {}
-    public static String InuZ() { return "Administrator"; }
-    public static String FwRs(String paramString1, String paramString2) { return null; }
+public final class FWZE {
+    public static final String ptrw = "Thanks for looking at the source. Please register Charles if you use it.";
+    public FWZE(String paramString1, String paramString2) {}
+    public static void ptrw(FWZE paramFWZE) {}
+    public static boolean ptrw() { return true; }
+    public static void poRY() {}
+    public static String Tvdd() { return "Administrator"; }
+    public static String ptrw(String paramString1, String paramString2) { return null; }
 }
 EOF
-javac -encoding UTF-8 Bvcn.java -d . && jar -uvf $CHARLES com/xk72/charles/Bvcn.class
+javac -encoding UTF-8 FWZE.java -d . && jar -uvf $CHARLES com/xk72/charles/FWZE.class
 cd .. && rm -rf $DIR
 xattr -d com.apple.quarantine /Applications/Charles.app/
