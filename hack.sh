165acc19a824ae333c1448b7468a0d2d2ba4b115
Roman Brovko <brovko-roman@ya.ru>
2020-11-15 19:44:32 +0200

    add support 4.6.1

--- hack.sh
+++ hack.sh
@@ -3,18 +3,18 @@ DIR=charleshack
 
 mkdir $DIR
 cd $DIR
-cat >> FWZE.java <<EOF
+cat >> MfoV.java <<EOF
 package com.xk72.charles;
-public final class FWZE {
-    public static final String ptrw = "Thanks for looking at the source. Please register Charles if you use it.";
-    public FWZE(String paramString1, String paramString2) {}
-    public static void ptrw(FWZE paramFWZE) {}
-    public static boolean ptrw() { return true; }
-    public static void poRY() {}
-    public static String Tvdd() { return "Administrator"; }
-    public static String ptrw(String paramString1, String paramString2) { return null; }
+public final class MfoV {
+    public static final String XdKP = "Thanks for looking at the source. Please register Charles if you use it.";
+    public MfoV(String paramString1, String paramString2) {}
+    public static void XdKP(MfoV paramMfoV) {}
+    public static boolean XdKP() { return true; }
+    public static void eCYm() {}
+    public static String uQqp() { return "Administrator"; }
+    public static String XdKP(String paramString1, String paramString2) { return null; }
 }
 EOF
-javac -encoding UTF-8 FWZE.java -d . && jar -uvf $CHARLES com/xk72/charles/FWZE.class
+javac -encoding UTF-8 MfoV.java -d . && jar -uvf $CHARLES com/xk72/charles/MfoV.class
 cd .. && rm -rf $DIR
 xattr -d com.apple.quarantine /Applications/Charles.app/
