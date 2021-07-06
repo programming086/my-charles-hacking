6b4f5a76ee6c2d15eb5f84e2f63673a2a1b6fabd
Roman Brovko <brovko-roman@ya.ru>
2021-07-06 18:51:30 +0300

    add support 4.6.2

--- hack.sh
+++ hack.sh
@@ -3,18 +3,18 @@ DIR=charleshack
 
 mkdir $DIR
 cd $DIR
-cat >> MfoV.java <<EOF
+cat >> p.java <<EOF
 package com.xk72.charles;
-public final class MfoV {
-    public static final String XdKP = "Thanks for looking at the source. Please register Charles if you use it.";
-    public MfoV(String paramString1, String paramString2) {}
-    public static void XdKP(MfoV paramMfoV) {}
-    public static boolean XdKP() { return true; }
-    public static void eCYm() {}
-    public static String uQqp() { return "Administrator"; }
-    public static String XdKP(String paramString1, String paramString2) { return null; }
+public final class p {
+    public static final String a = "Thanks for looking at the source. Please register Charles if you use it.";
+    public p(String paramString1, String paramString2) {}
+    public static void a(p paramp) {}
+    public static boolean a() { return true; }
+    public static void b() {}
+    public static String c() { return "Administrator"; }
+    public static String a(String paramString1, String paramString2) { return null; }
 }
 EOF
-javac -encoding UTF-8 MfoV.java -d . && jar -uvf $CHARLES com/xk72/charles/MfoV.class
+javac -encoding UTF-8 p.java -d . && jar -uvf $CHARLES com/xk72/charles/p.class
 cd .. && rm -rf $DIR
 xattr -d com.apple.quarantine /Applications/Charles.app/
