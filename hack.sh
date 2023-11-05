cddd288e72ca0703920aa7fe55f746fd5b83a4ba
Roman Brovko <brovko-roman@ya.ru>
2023-11-05 17:25:37 +0200

    add support 5.0b11

--- hack.sh
+++ hack.sh
@@ -3,17 +3,17 @@ DIR=charleshack
 
 mkdir $DIR
 cd $DIR
-cat >> MIDd.java <<EOF
+cat >> fXJQ.java <<EOF
 package com.xk72.charles;
-public final class MIDd {
-    public MIDd() {}
-    public static boolean CkAp() { return true; }
-    public static void uXfi() {}
-    public static String PrPI() { return "Administrator"; }
-    public static String CkAp(String paramString1, String paramString2) { return null; }
+public final class fXJQ {
+    public fXJQ() {}
+    public static boolean aJxm() { return true; }
+    public static void OzNQ() {}
+    public static String NXLy() { return "Administrator"; }
+    public static String aJxm(String paramString1, String paramString2) { return null; }
 }
 EOF
-javac -encoding UTF-8 MIDd.java -d . && jar -uvf $CHARLES com/xk72/charles/MIDd.class
+javac -encoding UTF-8 fXJQ.java -d . && jar -uvf $CHARLES com/xk72/charles/fXJQ.class
 cd .. && rm -rf $DIR
 xattr -d com.apple.quarantine /Applications/Charles.app/
 xattr -d com.apple.provenance /Applications/Charles.app/
